378248265484804fae7a5b08fd38187520ca8acd
CrazyDzen <ovevan89@gmail.com>
2018-12-07 12:30:00 +0300

    HW6
    К сожалению не успеваю даже лекцию просмотреть,поэтом сдаю код без изменени, на выходных надеюсь разобрать и сдать сразу два ДЗ, прошу пока отсрочить, выставление результатов по текуще работе.

---
+++ HW6/log/server_log_config.py
@@ -0,0 +1,11 @@
+import logging
+
+logging.basicConfig(
+    filename="ser_app.log",
+    format="%(asctime)s %(levelname)-10s %(module)s %(message)s",
+    level=logging.INFO
+)
+
+logger = logging.getLogger('basic')
+
+
